3cd29b3faabeed26232898701136063b405fd758
hankn01 <62194473+hankn01@users.noreply.github.com>
2020-05-24 00:37:55 +0900

    Output_Test_03
    TTTTTTTT
    TTTTTTTT
        TT  
        TT

---
+++ CPPPractice/Output_Test03.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	cout<<"TTTTTTTTTT"<<endl;
+	cout<<"TTTTTTTTTT"<<endl;
+	cout<<"    TT    "<<endl;
+	cout<<"    TT    "<<endl;	
+	return 0;
+}
