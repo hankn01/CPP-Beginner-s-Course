db922e7915f51b3388b2e8c79188139325c8f554
hankn01 <62194473+hankn01@users.noreply.github.com>
2020-05-25 20:28:41 +0900

    Multiplication Table cout Start and End num(cin)

---
+++ CPPPractice/Multiplication Table with Start and End num.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+int s, e;
+
+int i, j;
+cin>>s;
+cin>>e;
+
+if(s>9||s<2||e>9||e<2)
+{
+	cout<<"INPUT ERROR!";
+}
+if(s<=e)
+{
+for(i=s;i<e+1;i++)
+{
+	for(j=1;j<=9;j++)
+	{
+		cout<<i<<" * "<<j<<" = "<<" "<<i*j<<endl;
+	}
+}
+}
+else
+{
+	for(i=s;i>e-1;i--)
+{
+	for(j=1;j<=9;j++)
+	{
+		cout<<i<<" * "<<j<<" = "<<" "<<i*j<<endl;
+	}
+}
+}
+return 0;
+}
